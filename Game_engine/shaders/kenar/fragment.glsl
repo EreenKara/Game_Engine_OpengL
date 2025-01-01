99aad2be75ba56f5a8cf520ede1e7e395581b714
Yusuf Sedat Sagaltici <Yusufsdt222@gmail.com>
2025-01-01 21:25:43 +0300

    duzeltme

--- Game_engine/shaders/kenar/fragment.glsl
+++ Game_engine/shaders/kenar/fragment.glsl
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #version 330 core
 
 out vec4 FragColor; // Çıkış renk
@@ -6,12 +5,3 @@ out vec4 FragColor; // Çıkış renk
 void main() {
     FragColor = vec4(1.0, 1.0, 1.0, 1.0); // Beyaz renk
 }
-=======
-#version 330 core
-
-out vec4 FragColor; // Çıkış renk
-
-void main() {
-    FragColor = vec4(1.0, 1.0, 1.0, 1.0); // Beyaz renk
-}
->>>>>>> 6cd81b1acc66ad1619ef46524d3890d6a234ea51
--- Game_engine/shaders/kenar/vertex.glsl
+++ Game_engine/shaders/kenar/vertex.glsl
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #version 330 core
 
 layout(location = 0) in vec3 aPos; // Başlangıç noktası
@@ -8,14 +7,3 @@ void main()
 {
     gl_Position = uWorldTransform * vec4(aPos, 1.0);
 }
-=======
-#version 330 core
-
-layout(location = 0) in vec3 aPos; // Başlangıç noktası
-
-uniform mat4 uWorldTransform;
-void main()
-{
-    gl_Position = uWorldTransform * vec4(aPos, 1.0);
-}
->>>>>>> 6cd81b1acc66ad1619ef46524d3890d6a234ea51
