034e927b70f16d1165ed56902702c5ed87d9b190
Eren Kara <eren.kara2@ogr.sakarya.edu.tr>
2025-01-05 23:52:04 +0300

    farkli isik implementationu ama calismiyor, bakmak gerek

--- Game_engine/shaders/lightFarkli/fragment.glsl
+++ Game_engine/shaders/lightFarkli/fragment.glsl
@@ -12,16 +12,30 @@ uniform vec4 uAmbientLightColor= vec4(1.0f,1.0,1.0f,1.0f);       // isiklarin mi
 uniform vec4 uMaterialDiffuseColor = vec4(1.0f,0.0f,0.0f,1.0f);   // Cismin rengi
 uniform vec4 uMaterialAmbientColor = vec4(0.7f,0.7f,0.6f,1.0f);
 uniform vec4 uMaterialSpecularColor = vec4(0.7f,0.7f,0.6f,1.0f);
-uniform vec4 uCameraLocalPosition;
+uniform vec4 uCameraLocalPosition  =  vec4(1.0f,1.0,1.0f,1.0f); // buradaki kamera disardan gelmeli baslangic degeri olmamali. UGrasmamak ciin yaptim simdilik.
 void main()                                            
-{                  
+{         
+   float ambientFactor = 0.4f;
+
    vec3 normalizedNormal = normalize(Normal);
    vec3 normalizedLightDirection = normalize(LocalLightDirection);
    vec3 PixelToCamera  = normalize(uCameraLocalPosition-LocalPosition);
    float distance = dot(-normalizedLightDirection,normalizedNormal);
 
+   vec3 ReflectedLightDirection = normalizedNormal*2*distance + normalizedLightDirection;
 
-   vec4 diffuseColor = uMaterialDiffuseColor * uDirectionalLightColor *  Factor;                                    
+   float specularCos = dot(PixelToCamera,ReflectedLightDirection);
+
+   float specularFactor  = pow(max(specular,0.0),32);
+
+   float lightFactor = dot(-normalizedLightDirection, normalizedNormal);
+
+
    vec4 ambientColor = uMaterialAmbientColor * uAmbientLightColor *  uAmbientFactor;                                    
-   fragColor = texture( mySampler,TexCoord)*(diffuseColor+ambientColor);                                  
+   vec4 diffuseColor = uMaterialDiffuseColor * uDirectionalLightColor *  lightFactor;
+   vec4 specularColor = uMaterialDiffuseColor * uMaterialSpecularColor * specularFactor;
+   vec4 textureColor = texture( mySampler,TexCoord);
+
+
+   fragColor = textureColor*diffuseColor*vec4(1.0f,1.0f,1.0f,1.0f);                                  
 }                                                      
